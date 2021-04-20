e47a423c4d0f790a5c33deed11849f44d335aee7
node <gngenius02@gmail.com>
2021-04-20 18:29:10 +0000

    fixed bug when line didnt contain comma

--- main.go
+++ main.go
@@ -10,9 +10,13 @@ import (
 func scanner() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		line := strings.Split(scanner.Text(), ",")
-		if len(line[0]) > 1 && len(line[1]) > 1 {
-			fmt.Println("SET", line[1], line[0])
+		line := scanner.Text()
+		if strings.Contains(line, ",") {
+			line := strings.Split(line, ",")
+			if len(line[0]) > 1 && len(line[1]) > 1 {
+				fmt.Println("SET", line[1], line[0])
+			}
+
 		}
 	}
 }
